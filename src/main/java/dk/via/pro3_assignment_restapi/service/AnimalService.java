87ba3e4896c59ee9381a9b989a7657ea5539851d
Daniel <danielmaks400@gmail.com>
2025-11-04 11:04:15 +0100

    maybe maybe right implementation

--- src/main/java/dk/via/pro3_assignment_restapi/service/AnimalService.java
+++ src/main/java/dk/via/pro3_assignment_restapi/service/AnimalService.java
@@ -1,8 +1,35 @@
 package dk.via.pro3_assignment_restapi.service;
 
+import dk.via.pro3_assignment_restapi.model.Animal;
+import dk.via.pro3_assignment_restapi.repository.IAnimalRepository;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class AnimalService
 {
+    private IAnimalRepository animalRepository;
+
+    public Animal register(Animal animal){
+        animalRepository.save(animal);
+        return animal;
+    }
+    public List<Animal>  getAnimals(){
+        List<Animal> animals = animalRepository.findAll();
+        return animals;
+    }
+
+    public Animal getAnimalsById(int id){
+       // List<Animal> animals = animalRepository.findAll();
+    //return animals.stream().filter(animal -> animal.getRegNum() == id).findFirst().orElse(null);
+   return animalRepository.findById(id).get();
+    }
+
+ public List<Animal> getAnimalsByDate(String date){
+     LocalDate newDate = LocalDate.parse(date);
+    return animalRepository.findByDate(newDate);
+ }
+
 }
