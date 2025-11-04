b675b0bc79aa4bf19feaf784586c83a2af2c42a4
Daniel <danielmaks400@gmail.com>
2025-11-04 11:17:48 +0100

    maybe maybe right implementation maybge maybe

--- src/main/java/dk/via/pro3_assignment_restapi/repository/IAnimalRepository.java
+++ src/main/java/dk/via/pro3_assignment_restapi/repository/IAnimalRepository.java
@@ -1,11 +1,12 @@
 package dk.via.pro3_assignment_restapi.repository;
 
 import dk.via.pro3_assignment_restapi.model.Animal;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
-
+@Repository
 public interface IAnimalRepository
 {
   Animal save(Animal animal);
