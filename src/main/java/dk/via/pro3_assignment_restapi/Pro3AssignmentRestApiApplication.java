2a20f875160d5b11b81926f25650d2401f31b337
Smarties <basale95@gmail.com>
2025-11-04 09:51:18 +0100

    init

---
+++ src/main/java/dk/via/pro3_assignment_restapi/Pro3AssignmentRestApiApplication.java
@@ -0,0 +1,14 @@
+package dk.via.pro3_assignment_restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class Pro3AssignmentRestApiApplication
+{
+
+  public static void main(String[] args)
+  {
+    SpringApplication.run(Pro3AssignmentRestApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/dk/via/pro3_assignment_restapi/Pro3AssignmentRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.via.pro3_assignment_restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class Pro3AssignmentRestApiApplicationTests
+{
+
+  @Test void contextLoads()
+  {
+  }
+
+}
